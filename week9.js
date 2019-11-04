3d24b8a8877a64920bdc392fa5bb103937622f4e
gina Liu <43168953+gtl245@users.noreply.github.com>
2019-11-04 12:42:08 -0500

    Update week9.js

--- week9.js
+++ week9.js
@@ -1,13 +1,30 @@
+var switchtxt  = 1;
+var switchfont = 1;
+var pQuery = document.querySelector("p");
+
 document.addEventListener('keydown', theEvent => {
-  if (theEvent.keyCode === 32) {
-    alert("hello!");
+  if (theEvent.keyCode === 32 && switchfont === 1) {
+  	switchfont = 0;
+    pQuery.style.fontFamily = "georgia";
+  }
+  else if (theEvent.keyCode === 32 && switchfont === 0) {
+  	switchfont = 1;
+    pQuery.style.fontFamily = "courier";
   }
 })
 
-//onMouse event
-function mOn(obj){
-	obj.innerHTML="HELLOOO"
-}
-function mOut(obj){
-	obj.innerHTML="try hovering!"
+
+var txtbutton = document.querySelector("#button");
+txtbutton.addEventListener("click", changetxt); 
+
+function changetxt(){
+	if (switchtxt === 1){
+		pQuery.style.color = "#20B2AA";
+		switchtxt = 0;
+	}
+	else if (switchtxt === 0){
+		pQuery.style.color = "white" ;
+		switchtxt = 1;
+	}
+
 }
