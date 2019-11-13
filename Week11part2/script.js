f69ae9599977e2b6bd8aaa28cdeaa7af9a96a826
gina Liu <43168953+gtl245@users.noreply.github.com>
2019-11-13 16:21:32 -0500

    Add files via upload

---
+++ Week11part2/script.js
@@ -0,0 +1,19 @@
+
+//For adjectives
+$('#badj').click(function() {
+  var adj = $('#adj').val();
+  
+  $('#inputAdj').text(adj);
+});
+
+$('#bnoun1').click(function() {
+  var wnoun = $('#noun1').val();
+  
+  $('#inputNoun').text(wnoun);
+});
+
+$('#bnoun2').click(function() {
+  var wnoun2 = $('#noun2').val();
+  
+  $('#inputNoun2').text(wnoun2);
+});
