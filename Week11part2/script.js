b1d8f1a20569bb74768c7543b19c58d5252a2a1e
gina Liu <43168953+gtl245@users.noreply.github.com>
2019-11-13 19:27:07 -0500

    Update script.js

--- Week11part2/script.js
+++ Week11part2/script.js
@@ -1,17 +1,20 @@
-var adj = $('#adj').val();
-var wnoun = $('#noun1').val();
-var wnoun2 = $('#noun2').val();
+
 
 $('#badj').click(function() {
+  let adj = $('#adj').val();
   $('#inputAdj').text(adj);
+  
 });
 
 
 $('#bnoun1').click(function() {
+  let wnoun = $('#noun1').val();
+
   $('#inputNoun').text(wnoun);
 });
 
 
 $('#bnoun2').click(function() {
+  let wnoun2 = $('#noun2').val();
   $('#inputNoun2').text(wnoun2);
 });
