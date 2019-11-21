fc3922dd29ab99ad7590fe73ffc7b902c4db4590
gina Liu <43168953+gtl245@users.noreply.github.com>
2019-11-21 10:05:52 -0500

    Add files via upload

---
+++ Week12/draw.js
@@ -0,0 +1,25 @@
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index','-1');
+
+}
+
+
+function draw() {
+//  noStroke();
+  
+  r = random(0, 255);
+  g = random(0, 255);
+  b = random(0, 255);
+//  fill(r, g, b);
+//  ellipse(mouseX, mouseY, 5);
+  strokeWeight(5);
+  stroke(r, g, b);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
+
+function mousePressed() {
+  clear();
+}
