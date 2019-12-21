0f56390aca3ff818a253bb864e4a477521cd0f8d
gina Liu <43168953+gtl245@users.noreply.github.com>
2019-12-21 02:29:39 -0800

    Add files via upload

---
+++ MemoirsofMyNainai/17/draw.js
@@ -0,0 +1,29 @@
+/* code modified from  https://editor.p5js.org/ag3439/sketches/Skgh1ZQtQ */
+/* blinking stars */
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index','-1');
+  noCursor();
+  noStroke();
+  frameRate(5);
+  background(47,73,95); 
+}
+
+function draw() {
+  //background with transparancy
+  background(47,73,95,50); 
+  
+  //blinking stars
+  var galaxy = { 
+  locationX : random(width),
+  locationY : random(height),
+  size : random(windowWidth/180, windowHeight/250)
+}
+  ellipse(mouseX ,mouseY, galaxy.size, galaxy.size);
+  ellipse(galaxy.locationX ,galaxy.locationY, galaxy.size, galaxy.size);
+
+
+}
+
